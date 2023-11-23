a46d9d693e1f0c240880af8623d4e8996bd5c0b6
kimiko ando <kimiko.ando13@icloud.com>
2023-11-23 17:51:07 +0900

    maps are now working

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -5,16 +5,37 @@ import mapboxgl from 'mapbox-gl'
 export default class extends Controller {
   static values = {
     apiKey: String,
-    // markers: Array
+    markers: Array
   }
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
     container: this.element,
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
     // center: [-74.5, 40], // starting position [lng, lat]
     // zoom: 9, // starting zoom
     });
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
+
+  #addMarkersToMap(){
+    console.log(this.markersValue);
+    this.markersValue.forEach((marker) => {
+      const customMarker = document.createElement("div")
+      // customMarker.style.height: "30px"
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+      new mapboxgl.Marker()
+      .setLngLat([marker.lng, marker.lat])
+      .setPopup(popup)
+      .addTo(this.map);
+    })
   }
 }
