fd0a6cac0f1780bc8764aebdd59d40e7c328fc48
kimiko ando <kimiko.ando13@icloud.com>
2023-11-23 15:02:47 +0900

    updated the name of the map

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,4 +5,7 @@ class Place < ApplicationRecord
   validates :room_option, presence: true
   validates :langitude, presence: true
   validates :longitude, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
