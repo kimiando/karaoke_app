6af94f9fe2bfe39fa12818388f3c69c93cc3616d
Nick Lin <nk.shao0204@gmail.com>
2023-11-28 10:32:07 +0900

    change form design and other tiny stuff

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,7 @@
 class Playlist < ApplicationRecord
   belongs_to :user
   has_many :bookmarks, dependent: :destroy
+  has_many :songs, through: :bookmarks
   has_one_attached :photo
 
   validates :name, presence: true
