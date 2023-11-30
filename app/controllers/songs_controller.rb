70679b7dfc0ab066c4a9d8667ba6f4d70a9fc22a
kimiko ando <kimiko.ando13@icloud.com>
2023-11-30 11:13:40 +0900

    made changes in the song controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -40,8 +40,9 @@ class SongsController < ApplicationController
 
   def random
     # @song = Song.all.sample
-    session[:song_ids] = Song.favorites.pluck(:id) if session[:song_ids].empty?
+    session[:song_ids] = Song.favorites.pluck(:id) if session[:song_ids]&.empty?
     @song = Song.find(session[:song_ids].shift)
     @bookmark = Bookmark.new
+    # raise
   end
 end
