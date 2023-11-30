c55ae15beda40ec890e601ace2a6c7f6f620487d
kimiko ando <kimiko.ando13@icloud.com>
2023-11-30 16:42:11 +0900

    changed the code for the controller songs

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,12 +3,12 @@ class HomepageController < ApplicationController
     @recent_bookmarks = current_user.unique_bookmarks
     @recent_songs = Song.joins(:bookmarks).where(bookmarks: { id: @recent_bookmarks }).distinct.sort_by(&:created_at).reverse
     begin
-      @popular_songs = Playlist.find(77).songs
+      @popular_songs = Playlist.find(77).bookmarks
     rescue ActiveRecord::RecordNotFound
       @popular_songs = @recent_bookmarks
     end
     begin
-      @easy_songs = Playlist.find(78).songs
+      @easy_songs = Playlist.find(78).bookmarks
     rescue ActiveRecord::RecordNotFound
       @easy_songs = @recent_bookmarks
     end
